11c8908c1616b334aa00fd869efe79d6ef7073ef
hmsmurtaza <s.murtaza1@gmail.com>
2025-12-30 12:42:33 +0500

    Learned difference between covariance and invariance

--- src/main/kotlin/kotlin_generics/generic_classes/InvarianceAndCovarianceComparison.kt
+++ src/main/kotlin/kotlin_generics/generic_classes/InvarianceAndCovarianceComparison.kt
@@ -1,11 +1,20 @@
 package kotlin_generics.generic_classes
 
-// Invariance i.e Without out keyword
+// Invariance i.e Without 'out' keyword
 class BoxInvariant<T>(val item: T)
 
+class BoxCovariant<out T>(val item: T)  // If we add 'out' keyword
+
 fun main() {
+    //**************************** Invariant, without 'out' keyword *************************
     val stringBoxInvariant: BoxInvariant<String> = BoxInvariant("Assalamu-Alaikum")
 
     // Initializer type mismatch: expected type is BoxInvariant<Any>, actual type is BoxInvariant<String>
     // val anyBoxInvariant: BoxInvariant<Any> = stringBoxInvariant
+    //****************************************************************************************
+
+    //**************************** Covariant, with 'out' keyword *****************************
+    val stringBoxCovariant: BoxCovariant<String> = BoxCovariant("Walaikum-Slam")
+    // SUCCESS: This works perfectly!
+    val anyBoxCovariant: BoxCovariant<Any> = stringBoxCovariant
 }
