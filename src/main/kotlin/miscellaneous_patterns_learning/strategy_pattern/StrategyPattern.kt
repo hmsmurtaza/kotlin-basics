8ec8ec0a1ec31e511177d9e3c356a4ff718e001f
hmsmurtaza <s.murtaza1@gmail.com>
2026-01-05 13:23:58 +0500

    Learning strategy pattern

---
+++ src/main/kotlin/miscellaneous_patterns_learning/strategy_pattern/StrategyPattern.kt
@@ -0,0 +1,34 @@
+package miscellaneous_patterns_learning.strategy_pattern
+
+// That is the contract that all strategies must follow
+interface PaymentStrategy {
+    fun collectPaymentDetails()
+    fun pay(amount: Int)
+}
+
+// These are the actual implementation of the algorithm
+class CreditCardStrategy: PaymentStrategy {
+    override fun collectPaymentDetails() = println("Collecting credit card details...")
+    override fun pay(amount: Int) = println("Paid $amount using Credit Card.")
+}
+
+class PayPalStrategy : PaymentStrategy {
+    override fun collectPaymentDetails() = println("Logging into PayPal..")
+    override fun pay(amount: Int) = println("Paid $amount using PayPal.")
+}
+
+// This is the class that uses strategy
+class ShoppingCart(private var paymentStrategy: PaymentStrategy) {
+    fun setPaymentStrategy(newStrategy: PaymentStrategy) {
+        this.paymentStrategy = newStrategy
+    }
+
+    fun checkout(amount: Int) {
+        paymentStrategy.collectPaymentDetails()
+        paymentStrategy.pay(amount)
+    }
+}
+
+fun main() {
+
+}
