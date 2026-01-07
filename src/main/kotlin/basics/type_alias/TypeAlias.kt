83689adc917742c7cec04efbebce45848fdef1a6
hmsmurtaza <s.murtaza1@gmail.com>
2026-01-07 19:28:53 +0500

    Learned how to provide type of an argument without typealias keyword on runtime

--- src/main/kotlin/basics/type_alias/TypeAlias.kt
+++ src/main/kotlin/basics/type_alias/TypeAlias.kt
@@ -1,10 +1,19 @@
 package basics.type_alias
 
 typealias Predicate<T> = (T) -> Boolean
-
 fun foo(f: Predicate<Int>) = f(42)
 
+/**
+ * Following is the syntax if we don't use typealias keyword, and use/ provide type
+ * of the argument on runtime
+ */
+fun <K> foo1(f1: (K) -> Boolean) = f1(32 as K)
+
 fun main() {
-    println(foo { it > 0 })
+    println("With typealis keyword: ${foo { it > 0 }}")
 //    println(foo({it > 0}))    // Used with short lambda expression
+    /**
+     * Calling following method if we don't use typealias keyword
+     */
+    println("Without typealias keyword: ${foo1<Int> { it > 10 }}")
 }
